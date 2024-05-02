2d62365fd76ffd56618afa52ba712c940d037944
Palguien <pablo.poligl.1@educa.jcyl.es>
2024-05-02 11:50:15 +0200

    cambio

--- Libro/src/controlVersionesGit/ParaReventarV1.java
+++ Libro/src/controlVersionesGit/ParaReventarV1.java
@@ -3,9 +3,11 @@ package controlVersionesGit;
 public class ParaReventarV1 {
 
 	public static void main(String[] args) {
-		System.out.println("ultima version");
-
-
+		System.out.println("Hola mundo, versión 1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("ult");
+//cambio
 	}
 
 }
