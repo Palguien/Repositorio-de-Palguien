72389a95a26e079d94a43b85d887da6af42e64aa
junior <junior@DESKTOP-BRR4RM7>
2024-05-07 09:31:52 +0200

    ej 2 y 3 actualizado

--- Tema12-DynamicDataStructures/src/arrayList/ej2.java
+++ Tema12-DynamicDataStructures/src/arrayList/ej2.java
@@ -9,7 +9,7 @@ public class ej2 {
 	
 	public static void main(String args[]) {
 
-		ArrayList<String> dynamic = new ArrayList<String>();
+		ArrayList<String> lista = new ArrayList<String>();
 		Scanner keyBoard = new Scanner(System.in);
 		int count=0;
 
@@ -20,15 +20,15 @@ public class ej2 {
 		while(!aux.equals("salir")) {
 			System.out.println("mete un nombre (salir para terminar)");
 			aux=keyBoard.nextLine();
-			dynamic.add(aux);
+			lista.add(aux);
 			
 		}
-		dynamic.remove(dynamic.size()-1);
+		lista.remove(lista.size()-1);
 		
 		System.out.println("--Arraylist--");
 		//print ArrayList
-		for (int i = 0; i < dynamic.size(); i++) {
-			System.out.print(dynamic.get(i)+" - ");
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.print(lista.get(i)+" - ");
 		}
 		System.out.println();
 		System.out.println("-----------------------------------------");
@@ -41,6 +41,7 @@ public class ej2 {
 			System.out.println("2-Eliminar nombre");
 			System.out.println("3-Buscar nombre");
 			System.out.println("4-Ver lista");
+			System.out.println("5-Ordenar lista");
 			modo=keyBoard.nextInt();
 			keyBoard.nextLine();
 			switch(modo) {
@@ -50,13 +51,13 @@ public class ej2 {
 			case 1:
 				System.out.println("mete un nombre:");
 				aux=keyBoard.nextLine();
-				dynamic.add(aux);
+				lista.add(aux);
 				break;
 			case 2:
 				System.out.println("mete un nombre para eliminar:");
 				aux=keyBoard.nextLine();
-				if(dynamic.contains(aux)) {
-					dynamic.remove(aux);
+				if(lista.contains(aux)) {
+					lista.remove(aux);
 					System.out.println("eliminado");
 				}else {
 					System.out.println("el nombre introducido no está en la lista");
@@ -65,18 +66,23 @@ public class ej2 {
 			case 3:
 				System.out.println("mete un nombre para encontrar:");
 				aux=keyBoard.nextLine();
-				if(dynamic.contains(aux)) {			
-					System.out.println("está en la posición "+dynamic.indexOf(aux));
+				if(lista.contains(aux)) {			
+					System.out.println("está en la posición "+lista.indexOf(aux));
 				}else {
 					System.out.println("el nombre introducido no está en la lista");
 				}
 				break;
 			case 4:
 				//print ArrayList
-				for (int i = 0; i < dynamic.size(); i++) {
-					System.out.print(dynamic.get(i)+" - ");
+				for (int i = 0; i < lista.size(); i++) {
+					System.out.print(lista.get(i)+" - ");
 				}
 				System.out.println();
+				break;
+			case 5:
+				//ordenar
+				lista.sort(null);
+
 				break;
 			default:
 				System.out.println("Valor no válido");;
